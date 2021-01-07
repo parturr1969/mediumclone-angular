83222841752a525ec5d96f47771ec66fd1d03c4a
parturr <37025682+parturr1969@users.noreply.github.com>
2021-01-07 19:10:11 +0200

    Validate on register module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
-import {AppComponent} from './app.component';
-import {AppRoutingModule} from './app-routing.module';
-import {AuthModule} from './auth/auth.module';
+import {BrowserModule} from '@angular/platform-browser'
+import {NgModule} from '@angular/core'
+import {AppComponent} from './app.component'
+import {AppRoutingModule} from './app-routing.module'
+import {AuthModule} from './auth/auth.module'
 
 @NgModule({
   declarations: [AppComponent],
